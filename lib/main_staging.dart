ad1ab100e52f78985cc5b079f132bce827b11393
Ronak Patel <ronakpatel3105@gmail.com>
2025-02-01 20:51:07 +0530

    feat:create drawing app

---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:drawing_app/app/app.dart';
+import 'package:drawing_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
---
+++ test/app/view/app_test.dart
@@ -0,0 +1,12 @@
+import 'package:drawing_app/app/app.dart';
+import 'package:drawing_app/counter/counter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('App', () {
+    testWidgets('renders CounterPage', (tester) async {
+      await tester.pumpWidget(const App());
+      expect(find.byType(CounterPage), findsOneWidget);
+    });
+  });
+}
