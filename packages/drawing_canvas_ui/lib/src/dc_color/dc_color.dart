b246bdf84c749bd950c14604546cab231f05c62b
Ronak Patel <ronakpatel3105@gmail.com>
2025-02-03 17:46:54 +0530

    feat:Add dc_color

---
+++ packages/drawing_canvas_ui/lib/src/dc_color/dc_color.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+abstract class DCColor {
+  static const surfaceColor = Color(0XFF1E1E1E);
+  static const primaryColor = Color(0XFF42A5F5);
+  static const secondaryColor = Color(0XFF64B5F6);
+}
